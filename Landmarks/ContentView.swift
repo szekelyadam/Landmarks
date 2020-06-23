0765d8adb7d8d568e2a9767367d9a5e6e70f2e09
Ádám Székely <adam.szekely@prezi.com>
2020-06-23 15:45:23 +0200

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Ádám Székely on 2020. 06. 23..
+//  Copyright © 2020. Ádám Székely. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

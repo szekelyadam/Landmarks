a3326af9333fddb2903017ef0de514f92faf286f
Ádám Székely <adam.szekely@prezi.com>
2020-06-26 11:45:47 +0200

    Building lists and navigation - starting point

--- Landmarks/LandmarkDetail.swift
+++ Landmarks/LandmarkDetail.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LandmarkDetail.swift
 //  Landmarks
 //
 //  Created by Ádám Székely on 2020. 06. 23..
@@ -8,21 +8,21 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LandmarkDetail: View {
     var body: some View {
         VStack {
             MapView()
                 .edgesIgnoringSafeArea(.top)
                 .frame(height: 300)
-            
+
             CircleImage()
-                .offset(y: -130)
+                .offset(x: 0, y: -130)
                 .padding(.bottom, -130)
-            
+
             VStack(alignment: .leading) {
                 Text("Turtle Rock")
                     .font(.title)
-                HStack {
+                HStack(alignment: .top) {
                     Text("Joshua Tree National Park")
                         .font(.subheadline)
                     Spacer()
@@ -31,14 +31,14 @@ struct ContentView: View {
                 }
             }
             .padding()
-            
+
             Spacer()
         }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LandmarkDetail()
     }
 }
---
+++ Landmarks/LandmarkRow.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Ádám Székely on 2020. 06. 26..
+//  Copyright © 2020. Ádám Székely. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
